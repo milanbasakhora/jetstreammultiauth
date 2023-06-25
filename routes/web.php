b3aa89c6f64f7a165ae1933527156c8d2f844016
KirawaYo365 <kirawa.ya.files@gmail.com>
2023-06-25 13:18:05 +0545

    HomeController is created and modified accordingly

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Home Controller
+Route::get('/home',[HomeController::class,'redirect']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
